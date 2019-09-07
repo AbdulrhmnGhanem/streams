2666f8527263b79f48e86cf97d236d325d00ead5
Abdulrhman Ghanem <abdoghanem160@gmail.com>
2019-09-07 17:16:21 +0200

    control authentication state through button

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -26,13 +26,31 @@ class GoogleAuth extends React.Component{
 
     renderAuthButton() {
         if(this.state.isSignedIn === null) {
-            return <div>I don't know if we are signed in</div>
+            return null
         }else if(this.state.isSignedIn) {
-            return <div>I am signed in</div>
+            return (
+                <button className="ui red google button" onClick={this.onSignOutClick}>
+                    <i className="google icon"/>
+                    Sign Out
+                </button>
+            )
         }else {
-            return <div>I am not signed in</div>
+            return (
+                <button className="ui green google button" onClick={this.onSignInClick}>
+                    <i className="google icon"/>
+                    Sign In with google
+                </button>
+            )
         }
     }
+
+    onSignInClick = () => {
+        this.auth.signIn();
+    };
+
+    onSignOutClick = () => {
+        this.auth.signOut();
+    };
 
     render() {
         return (
