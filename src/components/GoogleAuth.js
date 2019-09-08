26b9a6de8a669d18cbe412c55c777262bc25f439
Abdulrhman Ghanem <abdoghanem160@gmail.com>
2019-09-08 10:55:12 +0200

    non functioning authentication

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import { connect } from 'react-redux';
+
+import  { signIn, signOut } from "../actions";
 
 
 class GoogleAuth extends React.Component{
-
-    state = {isSignedIn: null};
 
     componentDidMount() {
         // noinspection JSUnresolvedVariable
@@ -14,20 +15,24 @@ class GoogleAuth extends React.Component{
                 scope: 'email'
             }).then(() => {
                 this.auth = window.gapi.auth2.getAuthInstance();
-                this.onAuthChange();
+                this.onAuthChange(this.auth.isSignedIn.get());
                 this.auth.isSignedIn.listen(this.onAuthChange);
             });
         });
     }
 
-    onAuthChange = () => {
-        this.setState({isSignedIn: this.auth.isSignedIn.get()})
+    onAuthChange = isSignedIn => {
+        if ( isSignedIn ) {
+            this.props.signIn(this.auth.currentUser.get().getId());
+        }else {
+            this.props.signOut();
+        }
     };
 
     renderAuthButton() {
-        if(this.state.isSignedIn === null) {
+        if( this.props.isSignedIn === null ) {
             return null
-        }else if(this.state.isSignedIn) {
+        }else if( this.props.isSignedIn ) {
             return (
                 <button className="ui red google button" onClick={this.onSignOutClick}>
                     <i className="google icon"/>
@@ -59,4 +64,8 @@ class GoogleAuth extends React.Component{
     }
 }
 
-export default GoogleAuth;
+const mapStateToProps = state => {
+    return {isSignedIn: state.auth.isSignedIn}
+};
+
+export default connect( mapStateToProps , { signIn, signOut } )( GoogleAuth );
--- src/index.js
+++ src/index.js
@@ -1,9 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose } from "redux";
 
 import App from "./components/App";
+import reducers from './reducers'
+
+/* eslint-disable no-underscore-dangle */
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENTION_COMPOSE__ || compose;
+const store = createStore(
+    reducers,
+    composeEnhancers(applyMiddleware())
+    );
+/* eslint-enable */
 
 ReactDOM.render(
-<App/>,
+    <Provider store={store}>
+        <App/>
+    </Provider>,
     document.querySelector('#root')
 );
