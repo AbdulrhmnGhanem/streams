59045d999cecaf5b07cc61a83e49706f158804b1
Abdulrhman Ghanem <abdoghanem160@gmail.com>
2019-09-21 21:47:52 +0200

    Handle error message for stream creation form

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -4,14 +4,25 @@ import { Field, reduxForm } from "redux-form";
 
 class StreamCreate extends React.Component {
 
-    renderInput({ input, label }) {
+    renderError({ error, touched }) {
+        if (touched && error) {
+            return(
+                <div className="ui error message">
+                    <div className="header">{ error }</div>
+                </div>
+            )
+        }
+    }
+
+    renderInput= ({ input, label, meta }) => {
         return (
             <div className="field">
                 <label>{ label }</label>
-                <input {...input} />
+                <input { ...input } />
+                { this.renderError(meta) }
             </div>
             )
-    }
+    };
 
     onSubmit(formProps) {
         console.log(formProps)
@@ -19,7 +30,7 @@ class StreamCreate extends React.Component {
 
     render() {
         return (
-            <form onSubmit={ this.props.handleSubmit(this.onSubmit) } className="ui form">
+            <form onSubmit={ this.props.handleSubmit(this.onSubmit) } className="ui form error">
                 <Field name="title" component={ this.renderInput } label="Enter Title" />
                 <Field name="description" component={ this.renderInput } label="Enter Description" />
                 <button className="ui button primary">Submit</button>
@@ -28,6 +39,20 @@ class StreamCreate extends React.Component {
     }
 }
 
+const validate = formProps => {
+    const errors = {};
+
+    if (!formProps.title) {
+        errors.title = 'A stream should have a title'
+    }
+    if (!formProps.description) {
+        errors.description = 'A stream should have a description'
+    }
+
+    return errors
+};
+
 export default reduxForm({
-    form: 'streamCreate'
+    form: 'streamCreate',
+    validate
 })(StreamCreate);
