3264b625f58d1006c72e97e61afac5c866c33e6b
Abdulrhman Ghanem <abdoghanem160@gmail.com>
2019-09-22 22:10:03 +0200

    xRefactor StreamEdit component to use the general StreamForm component and pass init values

--- src/components/streams/StreamEdits.js
+++ src/components/streams/StreamEdits.js
@@ -1,8 +1,9 @@
 import React from "react";
 import { connect } from "react-redux";
+import _ from "lodash";
 
-import { fetchStream } from "../../actions";
-
+import { fetchStream, editStream } from "../../actions";
+import StreamForm from "./StreamForm";
 
 class StreamEdits extends React.Component {
 
@@ -10,8 +11,24 @@ class StreamEdits extends React.Component {
         this.props.fetchStream(this.props.match.params.id);
     }
 
+    onSubmit = (formValues) => {
+        this.props.editStream(this.props.match.params.id, formValues)
+    };
+
     render() {
-        return <div>edit</div>
+        if (!this.props.stream) {
+            return <div>Loading...</div>
+        }
+
+        return (
+            <div>
+                <h3>Edit Stream</h3>
+                <StreamForm
+                    onSubmit={ this.onSubmit }
+                    initialValues={ _.pick(this.props.stream, 'title', 'description') }
+                />
+            </div>
+        )
     }
 }
 
@@ -19,4 +36,4 @@ const mapStateToProps = (state, ownProps) => {
     return { stream: state.streams[ownProps.match.params.id] }
 };
 
-export default connect(mapStateToProps, { fetchStream })(StreamEdits);
+export default connect(mapStateToProps, { fetchStream, editStream })(StreamEdits);
